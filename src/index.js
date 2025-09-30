f917bec212a30556f7d6a3f22a505b423b1c4498
Hillary Tanui <hillarytanui80@gmail.com>
2025-09-30 21:24:09 +0300

    assignment complete

--- src/index.js
+++ src/index.js
@@ -1,1 +1,41 @@
 console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+document.addEventListener("DOMContentLoaded", function () {
+  fetchData();
+  fetchBreed();
+});
+
+const fetchData = () => {
+  const imgDiv = document.querySelector("#dog-image-container");
+  fetch(imgUrl)
+    .then((res) => res.json())
+    .then((data) => {
+      data.message.forEach((data) => {
+        imgDiv.innerHTML += `<img src='${data}' alt="">`;
+      });
+    });
+};
+const breedUrl = "https://dog.ceo/api/breeds/list/all";
+const fetchBreed = () => {
+  const ul = document.querySelector("#dog-breeds");
+  fetch(breedUrl)
+    .then((res) => res.json())
+    .then((data) => {
+      const breeds = Object.keys(data.message);
+      console.log(breeds);
+      breeds.forEach((data) => {
+        ul.innerHTML += `<li id='breed'>${data}</li>`;
+      });
+      const item = ul.querySelectorAll("#breed");
+      item.forEach((ite) => {
+        ite.addEventListener("click", function (e) {
+          e.target.style.color = "green";
+        });
+      });
+    });
+  const drop = document.querySelector("#breed-dropdown");
+  drop.addEventListener("click", function (e) {
+    alert(123);
+    console.log(e.target.value);
+  });
+};
